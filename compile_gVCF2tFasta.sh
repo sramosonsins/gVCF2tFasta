5a6dce0c719dc9ee2a02e93d49cf11a9f31fab17
Jordi Leno <jordi.leno@cragenomica.es>
2017-09-13 17:53:31 +0200

    compilation

--- compile_gVCF2tFasta.sh
+++ compile_gVCF2tFasta.sh
@@ -5,6 +5,6 @@ rm -rf build/Release/GNU-Linux/main.o
 make -f nbproject/Makefile-Release.mk build/Release/GNU-Linux/main.o
 mkdir -p build/Release/GNU-Linux
 rm -f "build/Release/GNU-Linux/main.o.d"
-g++ -std=c++0x -lz   -c -O2 -MMD -MP -MF "build/Release/GNU-Linux/main.o.d" -o build/Release/GNU-Linux/main.o main.cpp
+g++ -std=c++0x -lz   -c -O2 -MMD -MP -MF "build/Release/GNU-Linux/main.o.d" -o build/Release/GNU-Linux/main.o sources/main.cpp
 make -f Makefile CONF=Release
 
--- run_examples_gVCF2tFasta.sh
+++ run_examples_gVCF2tFasta.sh
@@ -14,7 +14,7 @@
 make -f nbproject/Makefile-Release.mk build/Release/GNU-Linux/main.o
 mkdir -p build/Release/GNU-Linux
 rm -f "build/Release/GNU-Linux/main.o.d"
-g++ -std=c++0x -lz   -c -O2 -MMD -MP -MF "build/Release/GNU-Linux/main.o.d" -o build/Release/GNU-Linux/main.o main.cpp
+g++ -std=c++0x -lz   -c -O2 -MMD -MP -MF "build/Release/GNU-Linux/main.o.d" -o build/Release/GNU-Linux/main.o sources/main.cpp
 /usr/bin/make -f Makefile CONF=Release
 
 cd ./Examples
